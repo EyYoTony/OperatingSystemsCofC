179c3e75310b4cb76c67aebf179632d1fecd42aa
Alexander Swanson <aswanson076@gmail.com>
2018-02-12 14:19:19 -0500

    notes and example for FDT

---
+++ c_programs/file_discriptor_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+void main(void) {
+  close(1);
+  int fd = open("out.txt", O_RDWR);  //fd returns the index of the opened file on the FDT
+  printf("This is the value of fd: %d\n", fd);
+  write(fd, "hello\n", 6); //Writes to out.txt
+}
+
+/*
+
+Table Defaults
+0 - stdin 
+1 - stdout
+2 - stderr
+
+*/
+
+/*
+
+execv ends the process and converts it to the command given while using the same FDT
+
+execv("/bin/ls", pargs)
+execv("/usr/bin/wc", cargs)
+
+*/
